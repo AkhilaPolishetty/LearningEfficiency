492ac1e555e84ae996f792ee00fb7160d917ff3a
Akhila Polishetty <akhilapolishetty@gmail.com>
2017-11-03 13:02:41 -0700

    Intersection of two arrays improved

--- IntersectionOfTwoArrays.java
+++ IntersectionOfTwoArrays.java
@@ -29,4 +29,39 @@ class Solution {
     }
 
 
+    //Improved version O(nlogn)
+     public int[] intersection2(int[] nums1, int[] nums2) {
+        
+        int len1 = nums1.length;
+        int len2 = nums2.length;
+        Arrays.sort(nums1);
+        HashSet<Integer> set = new HashSet<Integer>();
+        
+        for(Integer i : nums2){
+            boolean res = binarySearch(nums1, i);
+            if(res)
+               set.add(i);
+        }    
+        int[] result = new int[set.size()];
+        int index = 0;
+        for(Integer i : set){
+             result[index++] = i;
+            }
+        return result;
+        }
+    
+    
+    boolean binarySearch(int[] array, int target){
+        int len = array.length, l=0, r=len-1;
+        while(l<=r){
+            int m = l+(r-l)/2;
+            if(array[m] == target)
+                return true;
+            if(array[m] <target)
+                l= m+1;
+            else
+                r = m-1;
+        }
+        return false;
+    }
 }
